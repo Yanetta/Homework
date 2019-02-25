847cc2899cacb4151ebd39c50259e2bf2208ae33
Tetiana Pedchenko <36140882+Yanetta@users.noreply.github.com>
2019-02-25 12:09:16 +0200

    Update OrderDaoImplTest.java

--- src/test/java/OrderDaoImplTest.java
+++ src/test/java/OrderDaoImplTest.java
@@ -6,9 +6,10 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import static org.junit.Assert.assertTrue;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrderDaoImplTest {
     private OrderDaoImpl orderDao = new OrderDaoImpl();
-    Orders order = new Orders(BigDecimal.valueOf(123456), null,
+    Orders order = new Orders(BigDecimal.valueOf(123456), new Date(),
             BigDecimal.valueOf(2103), BigDecimal.valueOf(103), "REI", "41004",BigDecimal.valueOf(222), BigDecimal.valueOf(2000));
 
     @Test
