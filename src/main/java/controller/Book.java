b921bc4cc38cbad205ef8da7a59cce0c778d23ec
Yanetta <revenkot81@gmail.com>
2019-03-05 12:21:56 +0200

    commit on br31

---
+++ src/main/java/controller/Book.java
@@ -0,0 +1,44 @@
+package controller;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class Book {
+    private String name;
+    private int pages;
+
+    public Book() {
+    }
+
+    public Book(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
+}
+
+
